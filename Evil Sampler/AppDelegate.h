f334e8aca0963be3eac9c4334ea278fd20c35407
dave234 <dave234@users.noreply.github.com>
2016-02-16 20:04:02 -0800

    Initial Commit

---
+++ Evil Sampler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Evil Sampler
+//
+//  Created by david oneill on 2/16/16.
+//  Copyright © 2016 David O'Neill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Evil Sampler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Evil Sampler
+//
+//  Created by david oneill on 2/16/16.
+//  Copyright © 2016 David O'Neill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
